d9d114763d57eef25b6a98c17bd4275a34f185f0
andri-oc <a92231374@gmail.com>
2023-06-01 09:56:08 +0800

    last commit

--- admin/index.php
+++ admin/index.php
@@ -86,6 +86,32 @@
                                 include "../mahasiswa/mahasiswa_update.php";
 
                                 break;
+                                // Mata kuliah
+                            case "matakuliah-show";
+
+                                include "../matakuliah/matakuliah_show.php";
+
+                                break;
+                            case "matakuliah-add";
+
+                                include "../matakuliah/matakuliah_add.php";
+
+                                break;
+                            case "matakuliah-edit";
+
+                                include "../matakuliah/matakuliah_edit.php";
+
+                                break;
+                            case "matakuliah-delete";
+
+                                include "../matakuliah/matakuliah_delete.php";
+
+                                break;
+                            case "matakuliah-update";
+
+                                include "../matakuliah/matakuliah_update.php";
+
+                                break;
                                 // user
                             case "user-add";
                                 include "../user/user_add.php";

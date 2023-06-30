35f75796ff5c94fba71f53c130b77a69acdc2e94
Andri <96172300+andri-oc@users.noreply.github.com>
2023-06-30 17:43:45 +0800

    mengubah data matakuliah

--- admin/nav.php
+++ admin/nav.php
@@ -3,7 +3,7 @@
     <li class="list-group-item"><a href="?page=mahasiswa-show">Data Mahasiswa</a></li>
     <li class="list-group-item"><a href="?page=mahasiswa-add">Tambah Data Mahasiswa</a></li>
     <li class="list-group-item"><a href="?page=matakuliah-add">Tambah Data matakuliah</a></li>
-    <li class="list-group-item"><a href="?page=matakuliah-show">Tambah Data matakuliah</a></li>
+    <li class="list-group-item"><a href="?page=matakuliah-show">Data matakuliah</a></li>
     <?php
     if ($_SESSION['username'] == 'admin') {
         echo '<li class="list-group-item"><a href="?page=user-show">Data User</a></li>

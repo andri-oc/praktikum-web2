4d7853f3ebcaebc518f87e0802075334b1547688
Andri <96172300+andri-oc@users.noreply.github.com>
2023-06-30 17:44:03 +0800

    last commit

--- admin/nav.php
+++ admin/nav.php
@@ -1,9 +1,9 @@
 <ul class="list-group">
     <li class="list-group-item"><a href="?page=dashboard">Dashboard</a></li>
     <li class="list-group-item"><a href="?page=mahasiswa-show">Data Mahasiswa</a></li>
+    <li class="list-group-item"><a href="?page=matakuliah-show">Data matakuliah</a></li>
     <li class="list-group-item"><a href="?page=mahasiswa-add">Tambah Data Mahasiswa</a></li>
     <li class="list-group-item"><a href="?page=matakuliah-add">Tambah Data matakuliah</a></li>
-    <li class="list-group-item"><a href="?page=matakuliah-show">Data matakuliah</a></li>
     <?php
     if ($_SESSION['username'] == 'admin') {
         echo '<li class="list-group-item"><a href="?page=user-show">Data User</a></li>
